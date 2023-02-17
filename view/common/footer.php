1cf688c4bdad19934607f0b6c392cf9003544abf
Vladyslav Platonov <vladyslav_platonov@epam.com>
2023-02-17 19:53:45 +0200

    t

--- view/common/footer.php
+++ view/common/footer.php
@@ -3,7 +3,7 @@
         <div class="col-md-auto">
             <div class="copywrite">© 2023 <?php echo $ucp_settings['s_title']?> — Проект твого майбутнього.</div>
 			<a href='/offer'> — Договір та умови користувача</a>
-			<a href='/offer'> — Політика конфіденційності</a>
+			<a href='/policy'> — Політика конфіденційності</a>
             <img src="<?php echo $ucp_settings['s_logo']?>" class="footer-brand">
         </div>
         
